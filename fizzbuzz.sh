efce0d72dffcee8b55f5284bcb7e7650fe5ff7f5
Hummenix <36206434+Hummenix@users.noreply.github.com>
2019-07-10 21:07:28 +0200

    Create fizzbuzz.sh
    - POSIX compliant!
    - NEW compact code!

---
+++ fizzbuzz.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+###############################
+#######NOT MINE BUT NICE#######
+###############################
+
+start=1
+end=100
+
+fizz=3
+buzz=5
+
+count=$start
+while [ $count -le $end ]
+do
+    number=true
+    [ $(( (count-1)/fizz )) -lt $(( count/fizz )) ] && printf "Fizz" && number=false
+    [ $(( (count-1)/buzz )) -lt $(( count/buzz )) ] && printf "Buzz" && number=false
+    $number && printf "$count"
+    printf "\n"
+    count=$((count+1))
+done
