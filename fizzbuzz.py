5c7134ab26e6579718c9ed75f35b02d11507f217
Hummenix <diehummel88@gmail.com>
2019-07-10 18:08:35 +0200

    first commit

---
+++ fizzbuzz.py
@@ -0,0 +1,24 @@
+x = 0
+while x <= 100:
+    if x%3 == 0:
+        # fizz
+        fizz = True
+    else:
+        fizz = False
+
+    if x%5 == 0:
+        # buxx
+        buzz = True
+    else:
+        buzz = False
+
+    if fizz == True and buzz == True:
+        print("FizzBuzz")
+    elif fizz == True:
+        print("Fizz")
+    elif buzz == True:
+        print("Buzz")
+    else:
+        print(str(x))
+    x = x+1
+
