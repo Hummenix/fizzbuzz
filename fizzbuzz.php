801e83a62b5fe22f2a1fa93386dd22634dfaf248
Hummenix <diehummel88@gmail.com>
2019-07-11 03:57:17 +0200

    Added php version

---
+++ fizzbuzz.php
@@ -0,0 +1,31 @@
+<?php
+	echo "Hello World!";
+	$start = 0;
+	$end   = 100;
+	while ($start <= $end) {
+		#echo $start;
+		#Check for Fizz
+		if ($start%3==0) {
+			#Buzz
+			if ($start%5==0) {
+				echo "FizzBuzz\n";
+			}
+			else {
+				echo "Fizz\n";
+	
+			}
+		}
+		else {
+			#Buzz
+			if ($start%5==0) {
+				echo "Buzz \n";
+			}
+			else {
+				echo "$start \n";
+			}
+		}
+
+
+		$start = $start + 1;
+	}
+?>
