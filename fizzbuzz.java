493a249b6369770b2c5d4e5de2532bc77d6115a7
Hummenix <diehummel88@gmail.com>
2019-07-15 23:43:30 +0200

    added java variant

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -3,16 +3,13 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 0; i <= 100; i++) {
-            if (i%3 == 0 && i%5 == 0) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
-            }
-            else if (i%3 == 0) {
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            }
-            else if (i%5 == 0) {
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            }
-            else {
+            } else {
                 System.out.println(i);
             }
         }
